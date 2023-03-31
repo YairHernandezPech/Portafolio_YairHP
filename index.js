7262fc61bdbf86e60b643315bbcbe3466fa84ed6
YairHernandezPech <edilsonchelitohernandez011@gmail.com>
2023-03-31 12:42:37 -0600

    Mi primer commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function modoclaro() {
+    document.querySelector(".body").style='background: white;';
+    document.querySelector(".holasoy").style='color: black;';
+    document.querySelector(".info1").style='color: black;';
+    document.querySelector(".info2").style='color: black;';
+    document.querySelector(".info3").style='color: black;';
+}
+function modooscuro() {
+    document.querySelector(".body").style='background: #0F2027;background: -webkit-linear-gradient(to right, #2C5364, #203A43, #0F2027);background: linear-gradient(to right, #2C5364, #203A43, #0F2027);';
+    document.querySelector(".holasoy").style='color: white;';
+    document.querySelector(".info1").style='color: white;';
+    document.querySelector(".info2").style='color: white;';
+    document.querySelector(".info3").style='color: white;';
+}
