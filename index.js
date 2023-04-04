833c5e9683900b9b283b121f4d8c44e31cf6a653
YairHernandezPech <edilsonchelitohernandez011@gmail.com>
2023-04-03 20:13:58 -0600

    Mi primer commit

--- index.js
+++ index.js
@@ -12,20 +12,3 @@ function modooscuro() {
     document.querySelector(".info2").style='color: white;';
     document.querySelector(".info3").style='color: white;';
 }
-
-//Consumir api
-let url = "https://chatonline.fly.dev/chat/Msjenviar";
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => mostrarmensaje(data))
-  .catch((error) => console.log(error));
-
-  const mostrarmensaje = (data) => {
-    console.log(data);
-    let body = "";
-    for (var i = 0; i < data.length; i++) {
-      body += `<div class="comentarios"><div class="comentarioburbuja">${data[i].Msjenviar}</div></div>`;
-    }
-    document.getElementById("valores").innerHTML = body;
-    //console.log(body)
-  };
